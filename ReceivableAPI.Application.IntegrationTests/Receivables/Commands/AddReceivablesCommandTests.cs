0dadb3793aaa04edfd0c34a2d251c9db9d92343b
Michal Onyszko <monyszko@bravurasolutions.com>
2023-11-23 23:14:04 +0100

    Added simple integration tests

---
+++ ReceivableAPI.Application.IntegrationTests/Receivables/Commands/AddReceivablesCommandTests.cs
@@ -0,0 +1,45 @@
+﻿using ReceivablesAPI.Application.Common.Exceptions;
+using FluentAssertions;
+using ReceivablesAPI.Application.IntegrationTests.TestFeed;
+using ReceivablesAPI.Application.Receivables.Commands.AddReceivables;
+using ReceivablesAPI.Application.Receivables.Commands.AddReceivables.Dto;
+
+namespace ReceivablesAPI.Application.IntegrationTests.Receivables.Commands;
+
+
+using static Testing;
+
+public class AddReceivablesCommandTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireMinimumFields()
+    {
+        var command = new AddReceivablesCommand()
+        {
+            Receivables = new ReceivablesDto(){
+                ReceivableList = new List<ReceivableDto>(){
+                    
+                }
+            }
+        };
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<ValidationException>();
+    }
+
+    [Test]
+    public async Task ShouldAddReceivable()
+    {
+        var command = new AddReceivablesCommand()
+        {
+            Receivables = new ReceivablesDto(){
+                ReceivableList = new List<ReceivableDto>(){
+                    CorrectReceivableDtoFeed.GetTestInstance()
+                }
+            }
+        };
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().NotThrowAsync<ValidationException>();
+    }
+}
